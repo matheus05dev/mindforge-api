48e230df4643eca8d4fcdf639a1b6a30701a62d5
Matheus Nunes da Silva <matheus05.nunes81@gmail.com>
2026-01-28 20:40:06 -0300

    feat: Establish initial Spring Boot testing environment with H2 database, test-specific configurations, and a basic context load test.

--- src/test/java/com/matheusdev/mindforge/MindForgeApplicationTests.java
+++ src/test/java/com/matheusdev/mindforge/MindForgeApplicationTests.java
@@ -2,8 +2,10 @@ package com.matheusdev.mindforge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MindForgeApplicationTests {
 
 	@Test
